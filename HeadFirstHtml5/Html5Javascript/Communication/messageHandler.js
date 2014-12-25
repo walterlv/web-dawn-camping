f84e7747d3c93e42e05bfdb0bd82de236a88353a
walterlv <xblero@gmail.com>
2014-12-26 00:42:50 +0800

    Add a request page.
    Json file is still not found.

---
+++ HeadFirstHtml5/Html5Javascript/Communication/messageHandler.js
@@ -0,0 +1,18 @@
+﻿window.onload = function() {
+    var url = "http://localhost:4008/Communication/test.json";
+    var request = new XMLHttpRequest();
+    request.open("GET", url);
+    request.onload = function() {
+        if (request.status === 200) {
+            updateMessages(request.responseText);
+        } else if (request.status === 404) {
+            updateMessages("404: Page Not Found");
+        }
+    }
+    request.send(null);
+}
+
+function updateMessages(text) {
+    var messageDiv = document.getElementById("messages");
+    messageDiv.innerHTML = text;
+}
